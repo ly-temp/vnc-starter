5eb79a8727d9f76ace11bd6e035c651ccbad4bcb
ly-temp <100673133+ly-temp@users.noreply.github.com>
2024-04-10 19:29:47 +0800

    Add files via upload

--- init.sh
+++ init.sh
@@ -5,6 +5,5 @@ case $1 in
 		sudo yum install -y novnc tigervnc-server xorg-x11-server-Xvfb.x86_64 @xfce ;;
 esac
 
-openssl req -x509 -nodes -newkey rsa:2048 -keyout /etc/pki/tls/certs/novnc.pem -out /etc/pki/tls/certs/novnc.pem -days 365
-mkdir -p ~/.vnc
+sudo openssl req -x509 -nodes -newkey rsa:2048 -keyout /etc/pki/tls/certs/novnc.pem -out /etc/pki/tls/certs/novnc.pem -days 365
 echo 123456 | vncpasswd -f ~/.vnc/passwd && chmod 0600 ~/.vnc/passwd
