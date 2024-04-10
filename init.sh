8840c767998daaa5af82e9392ada3d6ed36a516f
ly-temp <100673133+ly-temp@users.noreply.github.com>
2024-04-10 19:25:18 +0800

    Add files via upload

--- init.sh
+++ init.sh
@@ -1,10 +1,8 @@
-#!/bin/sh
-
 #https://www.server-world.info/en/note?os=Fedora_26&p=desktop&f=8
 
 case $1 in
 	yum)
-		sudo yum install -y novnc xorg-x11-server-Xvfb.x86_64 @xfce ;;
+		sudo yum install -y novnc tigervnc-server xorg-x11-server-Xvfb.x86_64 @xfce ;;
 esac
 
 openssl req -x509 -nodes -newkey rsa:2048 -keyout /etc/pki/tls/certs/novnc.pem -out /etc/pki/tls/certs/novnc.pem -days 365
